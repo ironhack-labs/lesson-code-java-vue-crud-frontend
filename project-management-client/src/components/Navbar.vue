afb40fc0c6418f01fe4b11d7e746e2170ec786ab
unknown <raymond.maroun@ironhack.com>
2022-10-11 17:40:15 +0300

    Add project folder

---
+++ project-management-client/src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <nav className="Navbar">
+    <RouterLink to="/">
+      <button>Home</button>
+    </RouterLink>
+
+    <RouterLink to="/projects">
+      <button>Projects</button>
+    </RouterLink>
+  </nav>
+</template>
